2cf964d92a82f6a27e5894e8e9ce97580c325754
kaidoracer-dev <killian.lb@protonmail.com>
2026-01-16 18:45:51 +0100

    fix

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,2 +1,2 @@
 #!/usr/bin/python3
-print("{}".format("".join(chr(i)for i in range(ord('a'), ord('z') + 1) if chr(i) not in "qe"); end=""))
+print("{}".format("".join(chr(i)for i in range(ord('a'), ord('z') + 1) if chr(i) not in "qe"), end=""))
