fe0a2f09fbbe3b3a02bd577b313cdc9c94e3ced2
kaidoracer-dev <killian.lb@protonmail.com>
2026-01-16 18:48:15 +0100

    fix

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,2 +1,7 @@
 #!/usr/bin/python3
-print("{}".format("".join(chr(i)for i in range(ord('a'), ord('z') + 1) if chr(i) not in "qe"), end=""))
+letters = ""
+for i in range(ord('a'), ord('z') + 1):
+    if chr(i) not in "qe":
+        letters += chr(i)
+
+print(letters, end="")
