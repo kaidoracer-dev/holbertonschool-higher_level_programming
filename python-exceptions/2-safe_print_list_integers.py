6bb23a6cdcecc9ae232d1a682d36fe7345fa85d3
kaidoracer-dev <killian.lb@protonmail.com>
2026-01-23 14:30:37 +0100

    add files

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,2 +1,16 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
+    index = 0
+    count = 0
+    while index < x:
+        try:
+            print("{:d}".format(my_list[index]), end="")
+            count += 1
+            index += 1
+        except IndexError:
+            break
+        except (ValueError, TypeError):
+            pass
+            index += 1
+    print()
+    return count
