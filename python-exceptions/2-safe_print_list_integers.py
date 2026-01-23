3eefad4a32d8885191ad37394e3e9305ee4705f8
kaidoracer-dev <killian.lb@protonmail.com>
2026-01-23 14:33:53 +0100

    fix the file

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -6,11 +6,10 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[index]), end="")
             count += 1
-            index += 1
         except IndexError:
-            break
-        except (ValueError, TypeError):
             pass
+        except (ValueError, TypeError):
+            break
             index += 1
     print()
     return count
