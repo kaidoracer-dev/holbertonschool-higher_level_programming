f1b73ef78e337b19c8468593c9a64f5ef6012e48
kaidoracer-dev <killian.lb@protonmail.com>
2026-01-23 13:59:41 +0100

    fix the files

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -3,11 +3,11 @@ def safe_print_list(my_list=[], x=0):
     count = 0
     index = 0
     while count < x:
-        try: 
+        try:
             print(my_list[index], end="")
             count += 1
             index += 1
-        except:
+        except IndexError:
             break
     print()
     return (count)
