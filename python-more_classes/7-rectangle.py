85c68eea5c5418e68b4496155eb4b71689134101
kaidoracer-dev <killian.lb@protonmail.com>
2026-01-30 18:21:27 +0100

    fix

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -3,21 +3,25 @@
 
 
 class Rectangle:
+    """Defines a rectangle"""
 
     number_of_instances = 0
     print_symbol = "#"
 
     def __init__(self, width=0, height=0):
+        """Create rectangle and count instances"""
         self.width = width
         self.height = height
         Rectangle.number_of_instances += 1
 
     @property
     def width(self):
+        """Get width"""
         return self.__width
 
     @width.setter
     def width(self, value):
+        """Set width with checks"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
@@ -26,10 +30,12 @@ class Rectangle:
 
     @property
     def height(self):
+        """Get height"""
         return self.__height
 
     @height.setter
     def height(self, value):
+        """Set height with checks"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
@@ -37,9 +43,11 @@ class Rectangle:
         self.__height = value
 
     def area(self):
+        """Return area"""
         return self.__width * self.__height
 
     def perimeter(self):
+        """Return perimeter"""
         if self.width == 0 or self.height == 0:
             return 0
         return 2 * (self.__width + self.__height)
@@ -48,12 +56,14 @@ class Rectangle:
         """Return rectangle with print_symbol"""
         if self.width == 0 or self.height == 0:
             return ""
-        symbol = str(getattr(self, "print_symbol", Rectangle.print_symbol))
+        symbol = str(self.print_symbol)
         return "\n".join([symbol * self.width for _ in range(self.height)])
 
     def __repr__(self):
+        """Return string to recreate rectangle"""
         return "Rectangle({}, {})".format(self.width, self.height)
 
     def __del__(self):
+        """Delete instance message and decrease counter"""
         print("Bye rectangle...")
         Rectangle.number_of_instances -= 1
