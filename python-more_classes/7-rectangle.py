ef0a4ea1926ab6784d016b5d9dda11873d48bb20
kaidoracer-dev <killian.lb@protonmail.com>
2026-01-30 18:12:54 +0100

    fix

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -5,6 +5,7 @@
 class Rectangle:
 
     number_of_instances = 0
+    print_symbol = "#"
 
     def __init__(self, width=0, height=0):
         self.width = width
@@ -47,7 +48,8 @@ class Rectangle:
         """Return rectangle with print_symbol"""
         if self.width == 0 or self.height == 0:
             return ""
-        return "\n".join(["#" * self.width for _ in range(self.height)])
+        symbol = str(self.print_symbol)
+        return "\n".join([symbol * self.width for _ in range(self.height)])
 
     def __repr__(self):
         return "Rectangle({}, {})".format(self.width, self.height)
