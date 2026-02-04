fce339904b69ced0f2af7bd52610cd4194b21f24
kaidoracer-dev <killian.lb@protonmail.com>
2026-02-04 23:36:04 +0100

    fix for check

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""Define is_kind_of_class"""
+
+
 def is_kind_of_class(obj, a_class):
     """Return True if the object is an instance of the class or a
     subclass otherwise False"""
