20f28a3d2bee4250b600c728a1266693d76200d0
kaidoracer-dev <killian.lb@protonmail.com>
2026-02-11 19:31:07 +0100

    fix file 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -9,14 +9,13 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
-def main():
-    """script that adds all arguments to a Python list
-    and then save them to a file"""
-    try:
-        my_list = load_from_json_file("add_item.json")
-    except (FileNotFoundError, json.decoder.JSONDecodeError):
-        my_list = []
+"""script that adds all arguments to a Python list
+and then save them to a file"""
+try:
+    my_list = load_from_json_file("add_item.json")
+except (FileNotFoundError, json.decoder.JSONDecodeError):
+    my_list = []
 
-        my_list.extend(sys.argv[1:])
+my_list.extend(sys.argv[1:])
 
-        save_to_json_file(my_list, "add_item.json")
+save_to_json_file(my_list, "add_item.json")
