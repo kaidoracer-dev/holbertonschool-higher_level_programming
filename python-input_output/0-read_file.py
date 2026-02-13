e95f2526e041cde12351038a8549e964b8b6444a
kaidoracer-dev <killian.lb@protonmail.com>
2026-02-13 21:13:30 +0100

    fix file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-
-
 """Define"""
 
 
 def read_file(filename=""):
     """reads a text file UTF8 and prints it to stdout"""
     with open(filename, 'r', encoding="utf-8") as f:
-        print(f.read())
+        print(f.read(), end="")
