8c7b7dd223a6d367cb0eacb3080f20615918fef0
kaidoracer-dev <killian.lb@protonmail.com>
2026-03-06 18:13:45 +0100

    fix

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,4 @@
 -- Import the database dump from hbtn_0d_tvshows
-
 SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
 FROM tv_genres
 JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
