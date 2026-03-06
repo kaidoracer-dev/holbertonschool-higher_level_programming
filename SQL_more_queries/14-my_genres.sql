8a5f59dc34bc8e133d92d1f276b7a6910c0d8eee
kaidoracer-dev <killian.lb@protonmail.com>
2026-03-06 18:15:03 +0100

    fix

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,8 +1,7 @@
 -- Import the database dump from hbtn_0d_tvshows
-
 SELECT name
 FROM tv_genres
 JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
 WHERE tv_shows.title = 'Dexter'
-ORDER BY tv_genre.name ASC;
+ORDER BY tv_genres.name ASC;
