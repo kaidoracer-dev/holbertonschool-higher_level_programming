6649af584147a6635545d4c13a8a7a86c13edaa0
kaidoracer-dev <killian.lb@protonmail.com>
2026-03-06 18:03:43 +0100

    fix

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,7 @@
 -- Import the database dump from hbtn_0d_tvshows
 
-SELECT genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
 FROM tv_genres
-JOIN tv_show_genres ON tv_genres_id = tv_show_genres.genre_id
+JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY tv_genre.name
 ORDER BY number_of_shows DESC;
--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,6 +1,6 @@
 -- Import the database dump from hbtn_0d_tvshows
 
-SELECT tv.genres.name
+SELECT name
 FROM tv_genres
 JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
